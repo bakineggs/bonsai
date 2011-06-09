e5b5c2d757bbd6029b053b05a3e0d8e54ce53494
Dan Barry <dan@bakineggs.com>
2011-06-08 21:44:20 -0700

    refactor parsing nodes

--- parse.c
+++ parse.c
@@ -4,8 +4,14 @@
 
 #include "parse.h"
 
-Condition* parse_conditions(FILE* file);
 Rule* parse_rule(FILE* file);
+Condition* parse_conditions(FILE* file);
+Condition* parse_condition(char* line, Condition* previous); // TODO
+int condition_depth(Condition* condition); // TODO
+
+Node* parse_node(char* line, Node* previous);
+int node_depth(Node* node);
+
 void error(char* message, char* line);
 char* node_type_for(char** line_position, char* current_line);
 
@@ -151,120 +157,126 @@ Condition* parse_conditions(FILE* file) {
 }
 
 Node* parse_nodes(FILE* file) {
-  char current_line[80];
+  char line[80];
 
   do {
-    if (!fgets(current_line, 80, file)) {
+    if (!fgets(line, 80, file)) {
       if (ferror(file))
-        error("Error reading node", current_line);
+        error("Error reading node", line);
       else
-        error("No nodes specified", current_line);
+        error("No nodes specified", line);
     }
-  } while (strcmp(current_line, "\n") == 0);
+  } while (strcmp(line, "\n") == 0);
 
   Node* first = NULL;
-
-  Node* previous;
-  int previous_depth;
-  Node* current = NULL;
-  int current_depth = -1;
+  Node* node = NULL;
 
   do {
-    previous = current;
-    previous_depth = current_depth;
+    node = parse_node(line, node);
 
-    current = (Node*) malloc(sizeof(Node));
-    current->previous = NULL;
-    current->next = NULL;
-    current->children = NULL;
-    current->integer_value = NULL;
-    current->decimal_value = NULL;
-    current->string_value = NULL;
+    if (!first)
+      first = node;
 
-    char* line_position = current_line;
-
-    current_depth = 0;
-    while (strncmp(line_position, "  ", 2) == 0) {
-      current_depth++;
-      line_position += 2;
-    }
-
-    if (*line_position == ' ')
-      error("Space before node name", current_line);
-
-    if (current_depth == 0) {
-      if (!first)
-        first = current;
-      else {
-        while (previous->parent)
-          previous = previous->parent;
-        previous->next = current;
-        previous_depth = 0;
-      }
-      current->parent = NULL;
-    } else if (current_depth > previous_depth + 1)
-      error("Node at depth more than one level below its parent", current_line);
-    else {
-      while (previous_depth >= current_depth) {
-        previous = previous->parent;
-        previous_depth--;
-      }
-      current->parent = previous;
-
-      if (!previous->children) {
-        if (previous->integer_value || previous->decimal_value || previous->string_value)
-          error("Can't have child of node with a value", current_line);
-        previous->children = current;
-      } else {
-        Node* child = previous->children;
-        while (child->next)
-          child = child->next;
-        current->previous = child;
-        child->next = current;
-      }
-    }
-
-    current->type = node_type_for(&line_position, current_line);
-
-    if (*line_position++ != ':')
-      error("Missing : after node name", current_line);
-
-    if (current->ordered = *line_position == ':')
-      line_position++;
-
-    if (*line_position++ == ' ') {
-      if (current->ordered)
-        error("Ordered nodes can't have values", current_line);
-
-      if (*line_position >= '0' && *line_position <= '9') {
-        char* endptr;
-        current->integer_value = (long int*) malloc(sizeof(long int));
-        *current->integer_value = strtol(line_position, &endptr, 10);
-        if (*endptr == '.') {
-          free(current->integer_value);
-          current->integer_value = NULL;
-          current->decimal_value = (double*) malloc(sizeof(double));
-          *current->decimal_value = strtod(line_position, &endptr);
-        }
-        if (*endptr == ' ') {
-          while (*endptr == ' ')
-            endptr++;
-          if (*endptr != '#')
-            error("Expected comment after spaces at end of line", current_line);
-        }
-        if (*endptr != '\n')
-          error("Unexpected character after number", current_line);
-      } else {
-      }
-    }
-
-    if (!fgets(current_line, 80, file)) {
+    if (!fgets(line, 80, file)) {
       if (ferror(file))
-        error("Error reading node", current_line);
+        error("Error reading node", line);
       else
         return first;
     }
   } while (true);
+}
+
+Node* parse_node(char* line, Node* previous) {
+  Node* node = (Node*) malloc(sizeof(Node));
+  node->previous = NULL;
+  node->next = NULL;
+  node->children = NULL;
+  node->integer_value = NULL;
+  node->decimal_value = NULL;
+  node->string_value = NULL;
+
+  char* position = line;
+
+  int depth = 0;
+  while (strncmp(position, "  ", 2) == 0) {
+    depth++;
+    position += 2;
+  }
+
+  if (*position == ' ')
+    error("Space before node name", line);
+
+  if (depth == 0) {
+    if (previous) {
+      while (previous->parent)
+        previous = previous->parent;
+      previous->next = node;
+      node->previous = previous;
+    }
+    node->parent = NULL;
+  } else if (depth > node_depth(previous) + 1)
+    error("Node at depth more than one level below its parent", line);
+  else {
+    while (node_depth(previous) >= depth)
+      previous = previous->parent;
+    node->parent = previous;
+
+    if (!previous->children) {
+      if (previous->integer_value || previous->decimal_value || previous->string_value)
+        error("Can't have child of node with a value", line);
+      previous->children = node;
+    } else {
+      Node* child = previous->children;
+      while (child->next)
+        child = child->next;
+      node->previous = child;
+      child->next = node;
+    }
+  }
+
+  node->type = node_type_for(&position, line);
+
+  if (*position++ != ':')
+    error("Missing : after node name", line);
+
+  if (node->ordered = *position == ':')
+    position++;
+
+  if (*position++ == ' ') {
+    if (node->ordered)
+      error("Ordered nodes can't have values", line);
+
+    if (*position >= '0' && *position <= '9') {
+      char* endptr;
+      node->integer_value = (long int*) malloc(sizeof(long int));
+      *node->integer_value = strtol(position, &endptr, 10);
+      if (*endptr == '.') {
+        free(node->integer_value);
+        node->integer_value = NULL;
+        node->decimal_value = (double*) malloc(sizeof(double));
+        *node->decimal_value = strtod(position, &endptr);
+      }
+      if (*endptr == ' ') {
+        while (*endptr == ' ')
+          endptr++;
+        if (*endptr != '#')
+          error("Expected comment after spaces at end of line", line);
+      }
+      if (*endptr != '\n')
+        error("Unexpected character after number", line);
+    } else {
+      // TODO: parse string values
+    }
+  }
+
+  return node;
+}
+
+int node_depth(Node* node) {
+  int depth = 0;
+  while (node = node->parent)
+    depth++;
+  return depth;
 }
 
 void error(char* message, char* line) {
