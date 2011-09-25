6d00cfff327dd9a7c9c0f7787d1c8bfcd1d81a36
Dan Barry <dan@bakineggs.com>
2011-09-25 15:19:16 -0700

    specs about parsing rules

--- spec/compiler/parser_spec.rb
+++ spec/compiler/parser_spec.rb
@@ -36,9 +36,107 @@ describe Parser do
         Bar:
       EOS
     end
+
+    it 'treats a whole-line comment as a rule separator' do
+      parser.parse_rules(<<-EOS.gsub /^ {8}/, '').length.should == 2
+        Foo:
+        # comment
+        Bar:
+      EOS
+    end
   end
 
   describe '#parse_rule' do
+    it 'includes the top-level conditions' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:
+        Bar:
+      EOS
+      rule.conditions.length.should == 2
+    end
+
+    it 'nests descendant conditions' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:
+          Bar:
+            Baz:
+          Qux:
+        FooBar:
+      EOS
+      rule.conditions.length.should == 2
+
+      foo = rule.conditions[0].child
+      foo.node_type.should == 'Foo'
+      foo.conditions.length.should == 2
+
+      bar = foo.conditions[0].child
+      bar.node_type.should == 'Bar'
+      bar.conditions.length.should == 1
+
+      baz = bar.conditions[0].child
+      baz.node_type.should == 'Baz'
+      baz.conditions.should be_empty
+
+      qux = foo.conditions[1].child
+      qux.node_type.should == 'Qux'
+      qux.conditions.should be_empty
+
+      foo_bar = rule.conditions[1]
+      foo_bar.node_type.should == 'FooBar'
+      foo_bar.conditions.should be_empty
+    end
+
+    it 'considers the top level rule to have unordered conditions' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:
+      EOS
+      rule.conditions_are_ordered?.should be_false
+
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo::
+      EOS
+      rule.conditions_are_ordered?.should be_false
+    end
+
+    it 'considers one colon to mean conditions are unordered' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:
+      EOS
+      rule.conditions[0].child.conditions_are_ordered?.should be_false
+    end
+
+    it 'considers two colons to mean conditions are ordered' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo::
+      EOS
+      rule.conditions[0].child.conditions_are_ordered?.should be_true
+    end
+
+    it 'considers the top rule to not require conditions to match all nodes' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:
+      EOS
+      rule.requires_exact_match?.should be_false
+
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:=
+      EOS
+      rule.requires_exact_match?.should be_false
+    end
+
+    it 'considers lack of an equals sign to mean conditions do not have to match all nodes' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:
+      EOS
+      rule.conditions[0].child.requires_exact_match?.should be_false
+    end
+
+    it 'considers an equals sign to mean conditions have to match all nodes' do
+      rule = parser.parse_rule(<<-EOS.gsub /^ {8}/, '')
+        Foo:=
+      EOS
+      rule.conditions[0].child.requires_exact_match?.should be_true
+    end
   end
 
   describe '#parse_condition' do
