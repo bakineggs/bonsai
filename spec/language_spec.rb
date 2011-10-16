2b49a54d69c70bde4755af5d19a2d556cbc8179d
Dan Barry <dan@bakineggs.com>
2011-10-16 03:37:54 -0700

    start writing specs about language

---
+++ spec/language_spec.rb
@@ -0,0 +1,22 @@
+shared_examples_for 'an okk implementation' do
+  it 'errors out when no rules match' do
+    result = run_program :rules => "Foo:", :start_state => "Bar:"
+    result[:exit_status].should == 1
+    result[:stdout].should == ""
+    result[:stderr].should == "No rules to apply!\nBar:"
+  end
+
+  it 'errors out when no rules make a change' do
+    result = run_program :rules => "Foo:", :start_state => "Foo:"
+    result[:exit_status].should == 1
+    result[:stdout].should == ""
+    result[:stderr].should == "No rules to apply!\nFoo:"
+  end
+
+  it 'executes code of a matched rule' do
+    result = run_program :rules => "Foo: < exit(0);", :start_state => "Foo:"
+    result[:exit_status].should == 0
+    result[:stdout].should == ""
+    result[:stderr].should == ""
+  end
+end
