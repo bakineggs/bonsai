bc40f8895d86ffc071244d884ac0c13f31d0da92
Dan Barry <dan@bakineggs.com>
2011-10-23 03:25:39 -0700

    pseudo-code for a simple generated interpreter

--- compiler/compiler.rb
+++ compiler/compiler.rb
@@ -3,6 +3,19 @@ require File.dirname(__FILE__) + '/parser'
 class Compiler
   def compile program
     rules = Parser.new.parse_rules program
+
+    # Pseudo-code for generated interpreter:
+    #
+    # Nodes = [^], a poset in which all nodes come before their ancestors
+    #
+    # until Nodes is empty
+    #   Node = shift Nodes
+    #
+    #   if a rule can be, and is, applied to Node
+    #     add Node and its ancestors to Nodes
+    #   else
+    #     add Node's children to Nodes
+
     "int main() { return 0; }"
   end
 end
