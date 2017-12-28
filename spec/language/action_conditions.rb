6467a415cd00eb249b3dc0c80438b00c859ad3fe
Dan Barry <dan@bakineggs.com>
2017-12-29 09:23:17 +1300

    specs for inequality actions

--- spec/language/action_conditions.rb
+++ spec/language/action_conditions.rb
@@ -2,10 +2,18 @@ require_relative 'actions/addition'
 require_relative 'actions/subtraction'
 require_relative 'actions/multiplication'
 require_relative 'actions/division'
+require_relative 'actions/less_than'
+require_relative 'actions/greater_than'
+require_relative 'actions/less_than_or_equal_to'
+require_relative 'actions/greater_than_or_equal_to'
 
 RSpec.shared_examples 'action conditions' do
   include_examples 'addition action'
   include_examples 'subtraction action'
   include_examples 'multiplication action'
   include_examples 'division action'
+  include_examples 'less than action'
+  include_examples 'greater than action'
+  include_examples 'less than or equal to action'
+  include_examples 'greater than or equal to action'
 end
---
+++ spec/language/actions/greater_than_or_equal_to.rb
@@ -0,0 +1,233 @@
+RSpec.shared_examples 'greater than or equal to action' do
+  _it 'supplies the result of comparing equal integer values', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: 7
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'supplies the result of comparing a smaller integer value to a larger integer value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: 8
+        Result: X
+  EOR
+    Foo: 0
+  EOS
+
+  _it 'supplies the result of comparing a larger integer value to a smaller integer value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: 6
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'supplies the result of comparing equal decimal values', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7.0
+        Right: 7.0
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+
+  _it 'supplies the result of comparing a smaller decimal value to a larger decimal value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7.0
+        Right: 8.0
+        Result: X
+  EOR
+    Foo: 0
+  EOS
+
+
+  _it 'supplies the result of comparing a larger decimal value to a smaller decimal value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7.0
+        Right: 6.0
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'supplies the result of comparing equal integer and decimal values', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: 7.0
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'supplies the result of comparing equal decimal and integer values', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7.0
+        Right: 7
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'supplies the result of comparing a smaller integer value to a larger decimal value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: 7.001
+        Result: X
+  EOR
+    Foo: 0
+  EOS
+
+
+  _it 'supplies the result of comparing a larger integer value to a smaller decimal value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: 6.999
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'supplies the result of comparing a smaller decimal value to a larger integer value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 6.999
+        Right: 7
+        Result: X
+  EOR
+    Foo: 0
+  EOS
+
+  _it 'supplies the result of comparing a larger decimal value to a smaller integer value', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7.001
+        Right: 7
+        Result: X
+  EOR
+    Foo: 1
+  EOS
+
+  _it 'does not supply a left value that fits the inequality', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: X
+        Right: 7
+        Result: 1
+  EOR
+  EOS
+
+  _it 'does not supply a right value that fits the inequality', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: X
+    $:
+      >=:
+        Left: 7
+        Right: X
+        Result: 1
+  EOR
+  EOS
+
+  _it 'allows matching when the values fit the inequality', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: 7
+      +Bar: 8
+      +Baz: 0
+
+    ^:
+      Foo: Foo
+      Bar: Bar
+      Baz: Baz
+      !Matched:
+      +Matched:
+    $:
+      >=:
+        Left: Foo
+        Right: Bar
+        Result: Baz
+  EOR
+    Foo: 7
+    Bar: 8
+    Baz: 0
+    Matched:
+  EOS
+
+  _it 'prevents matching when the values violate the inequality', <<-EOR, <<-EOS
+    ^:
+      !Foo:
+      +Foo: 7
+      +Bar: 6
+      +Baz: 0
+
+    ^:
+      Foo: Foo
+      Bar: Bar
+      Baz: Baz
+      !Matched:
+      +Matched:
+    $:
+      >=:
+        Left: Foo
+        Right: Bar
+        Result: Baz
+  EOR
+    Foo: 7
+    Bar: 6
+    Baz: 0
+  EOS
+end
